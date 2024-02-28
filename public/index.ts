99e56f1e2c3c396fc7405e39cff890c1dff843fe
Shristi <shristijoshi03@gmail.com>
2024-02-29 01:54:52 +0545

    Add icons and images

---
+++ public/index.ts
@@ -0,0 +1,16 @@
+import SearchIcon from "./search.svg";
+import CartIcon from "./cart.svg";
+import FilterIcon from "./filter.svg";
+import GridIcon from "./grid_view.svg";
+import HomeIcon from "./home.svg";
+import TabIcon from "./tab.svg";
+import LockIcon from "./lock.svg";
+export {
+  SearchIcon,
+  CartIcon,
+  FilterIcon,
+  GridIcon,
+  HomeIcon,
+  TabIcon,
+  LockIcon,
+};

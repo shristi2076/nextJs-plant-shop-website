7cfcbe4d3812c7f0fbb1dd43de1f6076ed9ac05a
Shristi <shristijoshi03@gmail.com>
2024-02-28 10:10:39 +0545

    Add json file

---
+++ app/settings/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Settings = () => {
+  return (
+    <div className="h-fit flex items-center justify-center text-4xl">
+      Settings
+    </div>
+  );
+};
+
+export default Settings;
---
+++ utils/products.ts
@@ -0,0 +1,47 @@
+import snake from "@/public/snake-plant.jpg";
+import areca from "@/public/Areca.webp";
+
+export const products = [
+  {
+    id: 1,
+    src: areca,
+    alt: "No image found",
+    title: "Areca Plant",
+    price: "$220",
+  },
+  {
+    id: 2,
+    src: snake,
+    alt: "No image found",
+    title: "Snake Plant",
+    price: "$20",
+  },
+  {
+    id: 3,
+    src: areca,
+    alt: "No image found",
+    title: "Scoke Plant",
+    price: "$200",
+  },
+  {
+    id: 4,
+    src: snake,
+    alt: "No image found",
+    title: "Ochna Plant",
+    price: "$120",
+  },
+  {
+    id: 5,
+    src: areca,
+    alt: "No image found",
+    title: "Areca Plant",
+    price: "$100",
+  },
+  {
+    id: 6,
+    src: snake,
+    alt: "No image found",
+    title: "Zamia Plant",
+    price: "$150",
+  },
+];
